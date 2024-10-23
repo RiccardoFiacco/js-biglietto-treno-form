f17e354b64bb7f8836f0ae5d61f083ed8c569d53
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-23 15:51:23 +0200

    add function that  rreturn the ticket price

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //dichiare costante percentuale sconto u-18, sconto o-65 e con il prezzo per km
-const percentualeScontoU18 = 20;
-const percentualeScontoO65 = 40;
+const discountPercentageU18 = 20;
+const discountPercentageO65 = 40;
 const prezzoPerKm = 0.21;
 
 
@@ -13,28 +13,31 @@ form.addEventListener("submit", function(event){
     event.preventDefault();
     
     //vado a memorizzare dentro due variabilki rispettivamente km da percorrere e eta passeggero
-    let numeroKm = document.getElementById("km").value;
-    let eta = document.getElementById("age").value;
-    console.log(numeroKm, eta);
-    
+    let kmToTravel = document.getElementById("km").value;
+    let age = document.getElementById("age").value;
+    console.log(kmToTravel, age);
+    const price = priceCalc(kmToTravel, age);
+    console.log(price);
 })
 
+function priceCalc(km, age){
+    //calcolare prezzo base biglietto: km * costante 0,21
+    const basePrice = km * prezzoPerKm;
 
-//calcolare prezzo base biglietto: km * costante 0,21
-const prezzoBase = numeroKm * prezzoPerKm;
-//inizzializzo la variabile sconto
-let sconto = 0; 
-//SE eta < 18 
-    // sconto = prezzo * percentuale u-18 / 100
-//ALTRIMENTI se eta > 65 
-    // sconto = prezzo * percentuale o-65 / 100
+    //inizzializzo la variabile sconto
+    let discount = 0; 
 
-if(eta<18){
-    sconto = prezzoBase * percentualeScontoU18 / 100
-}else if(eta > 65){
-    sconto = prezzoBase * percentualeScontoO65 / 100
+    //SE eta < 18 
+        // sconto = prezzo * percentuale u-18 / 100
+    //ALTRIMENTI se eta > 65 
+        // sconto = prezzo * percentuale o-65 / 100
+    if(age<18){
+        discount = basePrice * discountPercentageU18 / 100;
+    }else if(age > 65){
+        discount = basePrice * discountPercentageO65 / 100;
+    }
+    //calcolo il prezzo finale 
+    let finalPrice = basePrice - discount;
+    //restituisco indietro il valore del prezzo finale
+    return finalPrice.toFixed(2);
 }
-
-let prezzoFinale = prezzoBase - sconto;
-
-console.log("prezzo finale: "+prezzoFinale.toFixed(2));
