68bae085d8baebd4306c8107f23380cb80630c9b
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-23 16:37:30 +0200

    add div to put the card and change postion to the funxction

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+function priceCalc(km, age){
+    //calcolare prezzo base biglietto: km * costante 0,21
+    const basePrice = km * prezzoPerKm;
+
+    //inizzializzo la variabile sconto
+    let discount = 0; 
+
+    //SE eta < 18 
+        // sconto = prezzo * percentuale u-18 / 100
+    //ALTRIMENTI se eta > 65 
+        // sconto = prezzo * percentuale o-65 / 100
+    if(age<18){
+        discount = basePrice * discountPercentageU18 / 100;
+    }else if(age > 65){
+        discount = basePrice * discountPercentageO65 / 100;
+    }
+    //calcolo il prezzo finale 
+    let finalPrice = basePrice - discount;
+    //restituisco indietro il valore del prezzo finale
+    return finalPrice.toFixed(2);
+}
+
 //dichiare costante percentuale sconto u-18, sconto o-65 e con il prezzo per km
 const discountPercentageU18 = 20;
 const discountPercentageO65 = 40;
@@ -20,7 +42,8 @@ form.addEventListener("submit", function(event){
     //memorizzo il prezzo che mi ritorna dalla funzione in una variabile
     const price = priceCalc(kmToTravel, age);
     console.log(price);
-    
+    generateHtml();
+
     //azzero tutti i valori
     document.getElementById("personal_info").value = '';
     document.getElementById("km").value='';
@@ -28,24 +51,3 @@ form.addEventListener("submit", function(event){
 
 })
 
-function priceCalc(km, age){
-    //calcolare prezzo base biglietto: km * costante 0,21
-    const basePrice = km * prezzoPerKm;
-
-    //inizzializzo la variabile sconto
-    let discount = 0; 
-
-    //SE eta < 18 
-        // sconto = prezzo * percentuale u-18 / 100
-    //ALTRIMENTI se eta > 65 
-        // sconto = prezzo * percentuale o-65 / 100
-    if(age<18){
-        discount = basePrice * discountPercentageU18 / 100;
-    }else if(age > 65){
-        discount = basePrice * discountPercentageO65 / 100;
-    }
-    //calcolo il prezzo finale 
-    let finalPrice = basePrice - discount;
-    //restituisco indietro il valore del prezzo finale
-    return finalPrice.toFixed(2);
-}
